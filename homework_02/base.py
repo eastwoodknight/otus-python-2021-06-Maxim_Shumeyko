d6209d05e7d3b468dcaacdeea414e4e2a991142f
Max_Shumeyko <eastwoodknight@yandex.ru>
2021-06-27 09:45:03 +0300

    homework_02

---
+++ homework_02/base.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from homework_02 import exceptions
+
+class Vehicle(ABC):
+    
+    def __init__(self, weight=0, started=0, fuel=0, fuel_consumption=0):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel;
+        self.fuel_consumption = fuel_consumption;
+        self.started = False
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                return 
+            raise exceptions.LowFuelError
+
+        print("Already started")
+
+    def move(self, distance):
+        total_consumption = self.fuel_consumption * distance
+        if self.fuel < total_consumption:
+            raise exceptions.NotEnoughFuel
+        self.fuel -= total_consumption
+        print(f"move {distance} km; fuel: {self.fuel}")
+            
---
+++ homework_02/plane.py
@@ -0,0 +1,23 @@
+"""
+создайте класс `Plane`, наследник `Vehicle`
+"""
+
+from homework_02.base import Vehicle
+from homework_02 import exceptions
+
+
+class Plane(Vehicle):
+
+    def __init__(self, weight=0, started=0, fuel=0, fuel_consumption=0, max_cargo=0):
+        super().__init__(weight, started, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+        self.cargo = 0
+
+    def load_cargo(self, cargo):
+        total_cargo = cargo + self.cargo
+        if total_cargo > self.max_cargo:
+            raise exceptions.CargoOverload
+        self.cargo = total_cargo
+        
+
+
