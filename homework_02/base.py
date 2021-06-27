1558c51e3c7b23ecb5ea048fcb2b08f976b9c16b
Max_Shumeyko <eastwoodknight@yandex.ru>
2021-06-27 12:03:49 +0300

    homework_02

--- homework_02/base.py
+++ homework_02/base.py
@@ -5,7 +5,6 @@ class Vehicle(ABC):
     
     def __init__(self, weight=0, fuel=0, fuel_consumption=0):
         self.weight = weight
-        self.started = started
         self.fuel = fuel;
         self.fuel_consumption = fuel_consumption;
         self.started = False
